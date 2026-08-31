218989cc9f622910e2036a5c3b671d2018ccc561
Shivam Mohan <shivammohan2403@gmail.com>
2026-08-31 19:39:17 +0530

    minor  changes

--- Assets/Assets/settings.py
+++ Assets/Assets/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-local-development-key-change-this"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
