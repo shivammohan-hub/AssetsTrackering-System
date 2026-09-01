eec47fbe4bc9daed959e725017508110548c2d16
Shivam Mohan <shivammohan2403@gmail.com>
2026-09-02 00:50:07 +0530

    updated

--- Assets/Assets/settings.py
+++ Assets/Assets/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-local-development-key-change-this"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
