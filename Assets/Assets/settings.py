b2c087cca20aad2cf98474f263f1c2fb33ffb807
Shivam Mohan <shivammohan2403@gmail.com>
2026-09-05 13:06:08 +0530

    Add media file configuration to settings; define MEDIA_URL and MEDIA_ROOT

--- Assets/Assets/settings.py
+++ Assets/Assets/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/mnt/data/media'
 
 
 # Email
